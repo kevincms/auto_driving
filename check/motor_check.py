bfb667078bc7305766bdfad55490a16309a21950
kevincms <kevincms314@naver.com>
2025-08-07 14:59:44 +0900

    motor topic 변경에 따른 코드와 파라미터 수정(모터 ros2->ros1 brige 등으로 출력 강화)

--- check/motor_check.py
+++ check/motor_check.py
@@ -2,16 +2,16 @@
 
 import rclpy, time
 from rclpy.node import Node
-from xycar_msgs.msg import XycarMotor
+from std_msgs.msg import Float32MultiArray
 
 class DriverNode(Node):
     def __init__(self):
         super().__init__('driver')
-        self.motor_publisher = self.create_publisher(XycarMotor, 'xycar_motor', 1)
-        self.motor_msg = XycarMotor()
+        self.motor_publisher = self.create_publisher(Float32MultiArray, 'xycar_motor', 1)
+        self.motor_msg = Float32MultiArray()
 
         # 파라미터 초기화
-        self.speed = 30
+        self.speed = 10
         self.angle = 0
         self.count = 0
         
@@ -20,19 +20,18 @@ class DriverNode(Node):
         self.create_timer(0.1, self.main_loop)
 
     def drive(self, angle, speed):
-        self.motor_msg.angle = float(angle)
-        self.motor_msg.speed = float(speed)
+        self.motor_msg.data = [float(angle), float(speed)]
         self.motor_publisher.publish(self.motor_msg)
 
     def main_loop(self):
         self.count+=1
-        if self.count==50:
-            if self.speed==30:
-                self.speed=10
-                self.angle=-20
+        if self.count%25==0:
+            if self.speed==10:
+                self.speed=5
+                self.angle=-self.angle
             else:
-                self.speed=30
-                self.angle=20
+                self.speed=10
+                self.angle=-self.angle
         self.drive(self.angle, self.speed)
             
 
